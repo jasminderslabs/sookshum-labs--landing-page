a43c526c8687e8122ef88e1a4e58e6e2aa2a541d
“[Jasminder <jasminder@sookshum-labs.com>
2026-02-02 17:26:59 +0530

    final commit after deploing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import appHostingAdapter from "@apphosting/astro-adapter";
+import icon from "astro-icon";
 
-import icon from 'astro-icon';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [icon()]
+  output: "server",
+  integrations: [icon()],
+
+  adapter: node({
+    mode: "standalone",
+    experimentalDisableStreaming: true,
+  }),
 });
